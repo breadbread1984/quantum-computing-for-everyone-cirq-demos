7d5c3cc05756d00d8fdf87c6359d3376ef257c8a
root <root@LAPTOP-A2EG90UI.localdomain>
2021-03-31 08:01:07 +0800

    update

--- bb84/bb84.py
+++ bb84/bb84.py
@@ -6,7 +6,7 @@ from absl import flags;
 from models import BB84;
 
 FLAGS = flags.FLAGS;
-flags.DEFINE_integer("key_length", 10, "expected key length");
+flags.DEFINE_integer("key_length", 5, "expected key length");
 
 def main():
 
@@ -16,8 +16,15 @@ def main():
 
   circuit = BB84(FLAGS.key_length * 4, alice_basis, bob_basis, alice_measures);
   result = cirq.Simulator().run(program = circuit, repetitions = 1);
-  obtained_key = [int(result.measurements[str(i)]) for i in range(FLAGS.key_length * 4) if alice_basis[i] == bob_basis[i]];
-  print(obtained_key);
+  alice_all_key = [alice_measures[i] for i in range(FLAGS.key_length * 4)];
+  bob_all_key = [int(result.measurements[str(i)]) for i in range(FLAGS.key_length * 4)];
+  alice_key = [alice_measures[i] for i in range(FLAGS.key_length * 4) if alice_basis[i] == bob_basis[i]];
+  bob_key = [int(result.measurements[str(i)]) for i in range(FLAGS.key_length * 4) if alice_basis[i] == bob_basis[i]];
+  
+  same = [(1 if alice_basis[i] == bob_basis[i] else 0) for i in range(FLAGS.key_length * 4)]
+  print(np.array(same))
+  print(np.array(alice_all_key));
+  print(np.array(bob_all_key));
 
 if __name__ == "__main__":
 
