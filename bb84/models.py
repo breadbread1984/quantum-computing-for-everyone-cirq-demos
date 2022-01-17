8b4f6ce8ae8fde3be551626b5341008ae4a49274
Konquer your Mind <breadbread1984@163.com>
2022-01-18 06:50:51 +0800

    Update models.py

--- bb84/models.py
+++ bb84/models.py
@@ -18,7 +18,7 @@ def BB84(qubit_num, alice_basises, bob_basises, eve_basises = None):
     # if eve presents
     if eve_basises is not None:
       circuit.append(cirq.I(qubits[idx]) if alice_basis == eve_basises[idx] else cirq.H(qubits[idx]));
-      circuit.append(cirq.measure_each(*qubits)); # eve measures
+      circuit.append(cirq.measure_each(qubits[idx])); # eve measures
       circuit.append(cirq.I(qubits[idx]) if eve_basises[idx] == bob_basis else cirq.H(qubits[idx]));
     else:
       # if alice's basis is the same as bob's basis the measures of both are the same, or measure the quabit from a perpendicular direction
