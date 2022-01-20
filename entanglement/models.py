e2a85e9164cb995da9caa2a0a7504707d2191c66
yi.xie <yi.xie@enflame-tech.com>
2022-01-20 23:11:26 +0800

    update

--- entanglement/models.py
+++ entanglement/models.py
@@ -9,8 +9,12 @@ def entangled_qubits_pair(measure_q1_first = True):
   circuit.append(cirq.ops.H(q1));
   circuit.append(cirq.ops.CNOT(q1, q2));
   # q1 odot q2 = 1/sqrt(2)*00> + 0*01> + 0*10> + 1/sqrt(2)*11>
-  circuit.append(cirq.ops.measure_each(q1));
-  circuit.append(cirq.ops.measure_each(q2));
+  if measure_q1_first:
+    circuit.append(cirq.ops.measure_each(q1));
+    circuit.append(cirq.ops.measure_each(q2));
+  else:
+    circuit.append(cirq.ops.measure_each(q2));
+    circuit.append(cirq.ops.measure_each(q1));
   return circuit;
 
 if __name__ == "__main__":
