3a37436c745ceeb8d4920ad193302b64f82ea8ae
yi.xie <yi.xie@enflame-tech.com>
2022-01-21 23:54:08 +0800

    update

--- bell_inequality/main.py
+++ bell_inequality/main.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
 
-from absl import app, flags;
 import cirq;
 from models import predefined_results, strange_results;
 
-FLAGS = flags.FLAGS;
-
-def add_options():
-  flags.DEFINE_boolean('predefined', default = False, help = 'whether the measure results are predefined');
-
-def main(unused_argv):
-  if FLAGS.predefined:
-    results, probability = predefined_results();
-    for key, results in results.items():
-      print(key, results);
-    print('if results are predefined, the measure is just exposing the results. then the probability of two measure results concide with each other is %f' % probability);
-  else:
-    probability = strange_results();
-    print('if results are defined at measure time. then the probability of two measure results concide with each other is %f' % probability);
+def main():
+  # 1) predinfed results
+  results, probability = predefined_results();
+  for key, results in results.items():
+    print(key, results);
+  print('if results are predefined, the measure is just exposing the results. then the probability of two measure results concide with each other is %f' % probability);
+  # 2) strange results
+  probability = strange_results();
+  print('if results are defined at measure time. then the probability of two measure results concide with each other is %f' % probability);
+  # 3) experimental results
+  
 
 if __name__ == "__main__":
-  add_options();
-  app.run(main);
+  main();
