4b3d667f981db2df4919e48dd4b0d0eabfaeb87f
yi.xie <yi.xie@enflame-tech.com>
2022-01-23 22:31:30 +0800

    update

---
+++ ekert/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import numpy as np;
+import cirq;
+from absl import flags;
+from models import ekert;
+
+FLAGS = flags.FLAGS;
+flags.DEFINE_integer('key_length', 5, "expected key length");
+flags.DEFINE_boolean('has_eve', False, "whether eve presents");
+
+def main(unused_argv):
+  alice_basises = np.random.randint(low = 0, high = 3, size = (FLAGS.key_length * 3,));
+  bob_basises = np.random.randint(low = 0, high = 3, size = (FLAGS.key_length * 3,));  eve_basises = np.random.randint(low = 0, high = 3, size = (FLAGS.key_length * 3,));
+  circuit = ekert(FLAGS.key_length * 3, alice_basises, bob_basises);
+  result = cirq.Simulator().run(program = circuit, repetitions = 1);
+
+if __name__ == "__main__":
+  app.run(main);
+
--- ekert/models.py
+++ ekert/models.py
@@ -3,7 +3,7 @@
 import numpy as np;
 import cirq;
 
-def ekert(qubit_num):
+def ekert(qubit_num, alice_basises, bob_basises):
   assert qubit_num % 3 == 0;
   alice_qubits = [cirq.devices.GridQubit(0, i) for i in range(qubit_num)];
   bob_qubits = [cirq.devices.LineQubit(1, i) for i in range(qubit_num)];
@@ -14,20 +14,19 @@ def ekert(qubit_num):
     circuit.append(cirq.ops.CNOT(alice_qubits[i], bob_qubits[i])));
   # q1 odot q2 = 1/sqrt(2)*00> + 1/sqrt(2)*11>
   # 2) measure with random basis
-  basises = np.random.randint(low = 0, high = 3, size = (2, qubit_num));
   for i in range(qubit_num):
-    if basises[0,i] == 0:
+    if alice_basises[i] == 0:
       circuit.append(cirq.ops.I(alice_qubits[i]));
-    elif basises[0,i] == 1:
+    elif alice_basises[i] == 1:
       circuit.append(cirq.ops.ry(-120/180*np.pi)(alice_qubits[i]));
-    elif basises[0,i] == 2:
+    elif alice_basises[i] == 2:
       circuit.append(cirq.ops.ry(-240/180*np.pi)(alice_qubits[i]));
     circuit.append(cirq.ops.measure_each(alice_qubits[i]));
-    if basises[1,i] == 0:
+    if bob_basises[i] == 0:
       circuit.append(cirq.ops.I(bob_qubits[i]));
-    elif basises[1,i] == 1:
+    elif bob_basises[i] == 1:
       circuit.append(cirq.ops.ry(-120/180*np.pi)(bob_qubits[i]));
-    elif basises[1,i] == 2:
+    elif bob_basises[i] == 2:
       circuit.append(cirq.ops.ry(-240/180*np.pi)(bob_qubits[i]));
     circuit.append(cirq.ops.measure_each(bob_qubits[i]));
   return circuit;
