b6d5312788123b2976c9c95c6903962be1e7d9ab
yi.xie <yi.xie@enflame-tech.com>
2022-01-24 14:06:11 +0800

    update

--- ekert/main.py
+++ ekert/main.py
@@ -3,7 +3,7 @@
 import numpy as np;
 import cirq;
 from absl import app, flags;
-from models import ekert;
+from models import ekert, same_measures_with_different_basises_probability;
 
 FLAGS = flags.FLAGS;
 flags.DEFINE_integer('key_length', 5, "expected key length");
@@ -13,7 +13,14 @@ def main(unused_argv):
   alice_basises = np.random.randint(low = 0, high = 3, size = (FLAGS.key_length * 3,));
   bob_basises = np.random.randint(low = 0, high = 3, size = (FLAGS.key_length * 3,));  eve_basises = np.random.randint(low = 0, high = 3, size = (FLAGS.key_length * 3,));
   circuit = ekert(FLAGS.key_length * 3, alice_basises, bob_basises);
+  # 1) alice and bob measure entangled qubit pair with their independendly selected basises
   result = cirq.Simulator().run(program = circuit, repetitions = 1);
+  # 2) both send their basises to each other, and choose the measures with the same basises
+  alice_measures_on_same_basis = [int(result.measurements['(0, %d)' % (i,)]) for i in range(FLAGS.key_length * 3) if alice_basises[i] == bob_basises[i]];
+  bob_measures_on_same_basis = [int(result.measurements['(1, %d)' % (i,)]) for i in range(FLAGS.key_length * 3) if alice_basises[i] == bob_basises[i]];
+  # 3) calculate the probability of both alice and bob get the same measure when they choose different basis
+  probability = same_measures_with_different_basises_probability();
+  print(probability)
 
 if __name__ == "__main__":
   app.run(main);
--- ekert/models.py
+++ ekert/models.py
@@ -3,6 +3,23 @@
 import numpy as np;
 import cirq;
 
+def same_measures_with_different_basises_probability():
+  probability = 0;
+  measure = lambda basis, superposition: np.dot(np.transpose(basis), superposition);
+  status = np.transpose([[1/np.sqrt(2), 1/np.sqrt(2)]]);
+  basises = [np.array([[1,0],[0,1]]),
+             np.array([[1/2, -np.sqrt(3)/2],[np.sqrt(3)/2, 1/2]]),
+             np.array([[-1/2, -np.sqrt(3)/2],[np.sqrt(3)/2, -1/2]]),];
+  for idx1, basis1 in enumerate(basises):
+    for idx2, basis2 in enumerate(basises):
+      if idx1 == idx2: continue; # skip measurement with a same basis
+      combination_probability = 1/len(basises) * 1/len(basises);
+      measure1 = measure(basis1, status); # measure1.shape = (2,1)
+      measure2 = measure(basis2, measure1); # measure2.shape = (2,1)
+      # NOTE: both measure results are 0 or 1
+      probability += combination_probability * (measure1[0,0]**2 * measure2[0,0]**2 + measure1[1,0]**2 * measure2[1,0]**2);
+  return probability;
+
 def ekert(qubit_num, alice_basises, bob_basises):
   assert qubit_num % 3 == 0;
   alice_qubits = [cirq.devices.GridQubit(0, i) for i in range(qubit_num)];
