f041fbde807d721a51a62aadb02ad301bc7db87c
yi.xie <yi.xie@enflame-tech.com>
2022-01-24 14:46:10 +0800

    update

--- ekert/main.py
+++ ekert/main.py
@@ -18,12 +18,13 @@ def main(unused_argv):
   # 2) both send their basises to each other, and choose the measures with the same basises
   alice_measures_on_same_basis = [int(result.measurements['(0, %d)' % (i,)]) for i in range(FLAGS.key_length * 3) if alice_basises[i] == bob_basises[i]];
   bob_measures_on_same_basis = [int(result.measurements['(1, %d)' % (i,)]) for i in range(FLAGS.key_length * 3) if alice_basises[i] == bob_basises[i]];
+  assert np.all(np.array(alice_measures_on_same_basis) == np.array(bob_measures_on_same_basis));
   # 3) calculate the probability of both alice and bob get the same measure when they choose different basis
   probability = same_measures_with_different_basises_probability();
   print("expected probability of both alice and bob get the same measure when they choose different basis is %f" % probability);
   alice_measures_on_different_basis = [int(result.measurements['(0, %d)' % (i,)]) for i in range(FLAGS.key_length * 3) if alice_basises[i] != bob_basises[i]];
   bob_measures_on_different_basis = [int(result.measurements['(1, %d)' % (i,)]) for i in range(FLAGS.key_length * 3) if alice_basises[i] != bob_basises[i]];
-  same_count = np.sum(np.array(alice_measures_on_different_basis == bob_measures_on_different_basis).astype(np.int32));
+  same_count = np.sum((np.array(alice_measures_on_different_basis) == np.array(bob_measures_on_different_basis)).astype(np.int32));
   total_count = len(alice_measures_on_different_basis);
   print('actual probability of both alice and bob get the same measure when they choose different basis is %f' % (same_count / total_count));
 
