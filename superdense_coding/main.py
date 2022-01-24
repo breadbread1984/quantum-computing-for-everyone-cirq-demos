7fbb684dbc07e780b3b91e5eca25fbf95635ee2e
breadbread1984 <breadbread1984@163.com>
2022-01-24 17:29:28 +0800

    update

--- superdense_coding/main.py
+++ superdense_coding/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from re import search;
 import numpy as np;
 import cirq;
 from absl import app, flags;
@@ -9,4 +10,18 @@ FLAGS = flags.FLAGS;
 flags.DEFINE_string('message', default = None, help = 'message to send');
 
 def main(unused_argv):
-  
+  assert search('[^01]', FLAGS.message) is None, "can only send string composed of 0 and 1";
+  assert len(FLAGS.message) % 2 == 0, "length of the message must be even";
+  circuit = superdense_coding(FLAGS.message);
+  result = cirq.Simulator().run(program = circuit, repetition = 1);
+  received_message = '';
+  for q1, q2 in zip(result.measurements['0'], result.measurements['1']):
+    if q1 == 0 and q2 == 0: received_message += '00';
+    elif q1 == 0 and q2 == 1: received_message += '01';
+    elif q1 == 1 and q2 == 0: received_message += '10':
+    elif q1 == 1 and q2 == 1: received_message += '11':
+  print('bob received message: %s' % received_message);
+
+if __name__ == "__main__":
+  app.run(main);
+
