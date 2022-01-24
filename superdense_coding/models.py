f60d9c6128a7254ff5962a0371a7adc7d65971c2
breadbread1984 <breadbread1984@163.com>
2022-01-24 16:11:17 +0800

    update

--- superdense_coding/models.py
+++ superdense_coding/models.py
@@ -13,7 +13,25 @@ def superdense_coding(message = None):
   for i in range(qubit_num):
     circuit.append(cirq.ops.H(alice_qubits[i]));
     circuit.append(cirq.ops.CNOT(alice_qubits[i], bob_qubits[i]));
+  # 2) alice send every 2bit by sending one qubit to bob
   for i in range(0,len(message),2):
     binary = message[i:i+2];
+    qidx = i // 2;
     if binary == '00':
-      circuit.append(cirq.ops.I(alice_qubits[i]));
+      circuit.append(cirq.ops.I(alice_qubits[qidx]));
+    elif binary == '01':
+      circuit.append(cirq.ops.X(alice_qubits[qidx]));
+    elif binary == '10':
+      circuit.append(cirq.ops.Z(alice_qubits[qidx]));
+    elif binary == '11':
+      circuit.append(cirq.ops.Y(alice_qubits[qidx]));
+    else:
+      raise Exception('invalid symbol in message');
+  # 3) bob receive 2bit per one qubit
+  for i in range(qubit_num):
+    circuit.append(cirq.ops.CNOT(alice_qubits[i], bob_qubits[i]));
+    circuit.append(cirq.ops.H(alice_qubits[i]));
+  circuit.append(cirq.ops.measure_each(*alice_qubits));
+  circuit.append(cirq.ops.measure_each(*bob_qubits));
+  return circuit;
+
