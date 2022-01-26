a728484376be731ea387649393ef4089f9b7acd6
breadbread1984 <breadbread1984@163.com>
2022-01-26 15:21:38 +0800

    updatee

---
+++ correction/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+from absl import flags, app;
+import cirq;
+from models import communication, correction;
+
+FLAGS = flags.FLAGS;
+
+flags.DFEINE_integer('qubit_num', 10, help = 'how many qubit to send');
+
+def main(unused_argv):
+  device = cirq.sim.Simulator();
+  circuit = communication(FLAGS.qubit_num);
+  result = device.run(program = circuit, repetitions = 1);
+  correction_code = '';
+  for i in range(FLAGS.qubit_num):
+    correction_code += int(result.measurements['(3, %d)' % i]);
+    correction_code += int(result.measurements['(4, %d)' % i]);
+  print('correction code is %s' % correction_code);
+  circuit = correction(correction_code);
+  result = device.run(program = circuit, repetitions = 1);
+  q1,q2,q3 = '','','';
+  for i in range(FLAGS.qubit_num):
+    q1 += int(result.measurements['(0, %d)' % i]);
+    q2 += int(result.measurements['(1, %d)' % i]);
+    q3 += int(result.measurements['(2, %d)' % i]);
+  print('q1: %s' % q1);
+  print('q2: %s' % q2);
+  print('q3: %s' % q3);
+
+
--- correction/models.py
+++ correction/models.py
@@ -6,15 +6,10 @@ import cirq;
 
 def communication(qubit_num):
   circuit = cirq.circuits.Circuit();
-  # 1) generate a triple of entangled qubits
+  # 1) generate q1 odot q2 odot q3 = 1/sqrt(2)*000>+1/sqrt(2)*111>
   for i in range(qubit_num):
-    dx = np.random.uniform(low = 0, high = np.pi);
-    dy = np.random.uniform(low = 0, high = np.pi);
-    dz = np.random.uniform(low = 0, high = 2 * np.pi);
-    # 1.1) generate a random status qubit
-    circuit.append(cirq.ops.rx(dx)(cirq.devices.GridQubit(0, i)));
-    circuit.append(cirq.ops.ry(dy)(cirq.devices.GridQubit(0, i)));
-    circuit.append(cirq.ops.rz(dz)(cirq.devices.GridQubit(0, i)));
+    # 1.1) generate q1 = 1/sqrt(2)*0>+1/sqrt(2)*1>
+    circuit.append(cirq.ops.H(cirq.devices.GridQubit(0, i)));
     # 1.2) entangle first and second qubit
     circuit.append(cirq.ops.CNOT(cirq.devices.GridQubit(0, i), cirq.devices.GridQubit(1, i)));
     # 1.3) entangle first and thrid qubit
@@ -29,6 +24,13 @@ def communication(qubit_num):
     circuit.append(cirq.ops.CNOT(cirq.devices.GridQubit(0, i), cirq.devices.GridQubit(4, i)));
     # 2.4) entangle thrid qubit and second correction qubit
     circuit.append(cirq.ops.CNOT(cirq.devices.GridQubit(2, i), cirq.devices.GridQubit(4, i)));
+  # 3) create random flip to one of the first three qubits
+  for i in range(qubit_num):
+    qidx = np.random.randint(low = 0, high = 3);
+    circuit.append(cirq.ops.X(cirq.devices.GridQubit(qidx, i)));
+  for i in range(qubit_num):
+    circuit.append(cirq.ops.measure_each(cirq.devices.GridQubit(3, i)));
+    circuit.append(cirq.ops.measure_each(cirq.devices.GridQubit(4, i)));
   return circuit;
 
 def correction(correction_code):
@@ -44,5 +46,9 @@ def correction(correction_code):
       circuit.append(cirq.ops.X(cirq.devices.GridQubit(1, i)));
     elif cc == '01':
       circuit.append(cirq.ops.X(cirq.devices.GridQubit(2, i)));
+  for i in range(qubit_num):
+    circuit.append(cirq.ops.measure_each(cirq.devices.GridQubit(0, i)));
+    circuit.append(cirq.ops.measure_each(cirq.devices.GridQubit(1, i)));
+    circuit.append(cirq.ops.measure_each(cirq.devices.GridQubit(2, i)));
   return circuit;
 
