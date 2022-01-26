601afacdea32ad935304592687c52298ca5be5a1
breadbread1984 <breadbread1984@163.com>
2022-01-26 15:26:10 +0800

    update

--- correction/main.py
+++ correction/main.py
@@ -14,16 +14,16 @@ def main(unused_argv):
   result = device.run(program = circuit, repetitions = 1);
   correction_code = '';
   for i in range(FLAGS.qubit_num):
-    correction_code += int(result.measurements['(3, %d)' % i]);
-    correction_code += int(result.measurements['(4, %d)' % i]);
+    correction_code += result.measurements['(3, %d)' % i];
+    correction_code += result.measurements['(4, %d)' % i];
   print('correction code is %s' % correction_code);
   circuit = correction(correction_code);
   result = device.run(program = circuit, repetitions = 1);
   q1,q2,q3 = '','','';
   for i in range(FLAGS.qubit_num):
-    q1 += int(result.measurements['(0, %d)' % i]);
-    q2 += int(result.measurements['(1, %d)' % i]);
-    q3 += int(result.measurements['(2, %d)' % i]);
+    q1 += result.measurements['(0, %d)' % i];
+    q2 += result.measurements['(1, %d)' % i];
+    q3 += result.measurements['(2, %d)' % i];
     assert q1[-1] == q2[-1] and q2[-1] == q3[-1], 'correction failed';
   print('q1: %s' % q1);
   print('q2: %s' % q2);
