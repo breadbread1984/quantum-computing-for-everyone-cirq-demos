11fad1bbcbfef7cf679e6acb001e7c871f3689a8
yi.xie <yi.xie@enflame-tech.com>
2022-01-28 21:43:20 +0800

    update

--- deutsch/main.py
+++ deutsch/main.py
@@ -10,7 +10,7 @@ flags.DEFINE_boolean('use_balance', default = False, help = 'whether use balance
 
 def main(unused_argv):
   circuit = deutsch(FLAGS.use_balance);
-  result = cirq.sim.Simulator.run(program = circuit, repetitions = 1);
+  result = cirq.sim.Simulator().run(program = circuit, repetitions = 1);
   print('measure %d' % int(result.measurements['0']));
 
 if __name__ == "__main__":
