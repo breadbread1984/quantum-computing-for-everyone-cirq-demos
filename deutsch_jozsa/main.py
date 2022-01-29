8a92ed595ce2e651e9b46b7abbc0893e3f803543
yi.xie <yi.xie@enflame-tech.com>
2022-01-30 07:42:48 +0800

    update

--- deutsch_jozsa/main.py
+++ deutsch_jozsa/main.py
@@ -8,23 +8,19 @@ from models import deutsch_jozsa;
 FLAGS = flags.FLAGS;
 
 flags.DEFINE_boolean('use_balance', default = False, help = 'whether use balance function as oracle');
-flags.DEFINE_integer('n_args', default = 3, help = 'number of arguments of the function');
-flags.DEFINE_integer('repetitions', default = 10, help = 'maximum measure repetitions before make predicate');
+flags.DEFINE_integer('n_args', default = 6, help = 'number of arguments of the function');
 
 def main(unused_argv):
   circuit = deutsch_jozsa(FLAGS.n_args, FLAGS.use_balance);
-  result = cirq.sim.Simulator().run(program = circuit, repetitions = FLAGS.repetitions);
-  measures = ['' for i in range(FLAGS.repetitions)];
+  result = cirq.sim.Simulator().run(program = circuit, repetitions = 1);
+  measure = '';
   for i in range(FLAGS.n_args):
-    for r in range(FLAGS.repetitions):
-      measures[r] += str(int(result.measurements['%d' % i][r]));
-  print('measures of qubits in %d repetitions are ' % FLAGS.repetitions, measures);
-  for measure in measures:
-    if search('[^0]', measure) is not None:
-      print('the function is a balance function with error probability 0');
-      return;
-  print('the function is a constant function with error probability %f' % (0.5**FLAGS.repetitions));
-  return;
+    measure += str(int(result.measurements['%d' % i]));
+  print('measures of qubits are ' % measure);
+  if search('[^0]', measure) is not None:
+    print('the function is a balance function');
+  else:
+    print('the function is a constant function');
   
 if __name__ == "__main__":
   app.run(main);
--- deutsch_jozsa/models.py
+++ deutsch_jozsa/models.py
@@ -10,12 +10,9 @@ def const_func(circuit, qubits, q):
   circuit.append(cirq.ops.I(q));
 
 def balance_func(circuit, qubits, q):
-  # NOTE: no readily available CCNOT for multiple qubits (over 2), so I have to create the gate manually
-  n = len(qubits) + 1;
-  ccnot = np.eye(2**n);
-  ccnot = np.concatenate([ccnot[:,:-2], ccnot[:,-1:], ccnot[:,-2:-1]], axis = 1);
+
   # balance function
-  circuit.append(cirq.ops.MatrixGate(ccnot)(*qubits,q));
+  circuit.append(cirq.ops.CNOT(qubits[-1],q));
 
 def deutsch_jozsa(n, use_balance = True):
   # NOTE: no readily available Hadamard for multiple qubits, so I have to create the gate with kronecker
